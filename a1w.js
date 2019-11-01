406a2caaa9ccf6719a4eca240eaf6d9ea324c5b1
Joe Hitchens <joe@sleepless.com>
2019-10-31 18:24:34 -0700

    -

--- a1w.js
+++ a1w.js
@@ -6,43 +6,50 @@ request = require( "request" );
 A1W = {}
 
 if( module !== undefined ) {
-	module.exports = A1W;		 // node.js
+	// node
+
+	module.exports = A1W;
+
+	A1W.http_get = function( url, cb ) {
+		request( url, ( err, resp, body ) => {
+			cb( body, err );
+		});
+	};
+
 }
+else {
+	// browser
+
+	A1W.http_get = function( url, cb ) {
+		let x = new XMLHttpRequest();
+		x.addEventListener( "load", function() {
+			cb( x.response, null );
+		});
+		x.addEventListener( "error", function( evt ) {
+			cb( null, evt );
+		});
+		x.open("GET", url);
+		x.send();
+	};
+
+}
+
 
 A1W.google = {
 	drive: {
 		document: {
 			get: function( doc_id, cb ) {
 				let url = "https://sleepless.com/api/?cmd=google/drive/document&key=" + doc_id;	// XXX endpoint is not final
-				request( url, ( err, resp, body ) => {
-					cb( body, err, resp );
-				});
+				A1W.http_get( url, cb );
 			},
 		},
 		spreadsheet: {
 			get: function( doc_id, cb ) {
 				let url = "https://sleepless.com/api/?cmd=google/drive/spreadsheet&key=" + doc_id;	// XXX endpoint is not final
-				request( url, ( err, resp, body ) => {
-					cb( body, err, resp );
-				});
+				A1W.http_get( url, cb );
 			},
 		},
 	},
-};
-A1W.sleepless = {
-	files: {
-		get: function( authkey, file, cb ) {
-			let url = "https://a1widgets.com/api/v1/sleepless/files/"; // XXX endpoint is not final
-			url += "?authkey=" + encodeURIComponent( authkey );
-			url += "&file=" + encodeURIComponent( file );
-			request( url, ( err, resp, body ) => {
-				cb( body, err, resp );
-			});
-		},
-		put: function( auth_key, path, cb ) {
-			throw new Error( "not implemented" );
-		},
-	}
 };
 
 
