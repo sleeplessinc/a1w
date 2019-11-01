9df07ff4508056990c49c72d6d4152081aa31472
Joe Hitchens <joe@sleepless.com>
2019-10-31 21:11:50 -0700

    -

--- a1w.js
+++ a1w.js
@@ -1,14 +1,14 @@
 
 // Copyright 2019 Sleepless Software Inc. All Rights Reserved
 
-request = require( "request" );
-
 A1W = {}
 
-if( module !== undefined ) {
+if( typeof process !== 'undefined' ) {
 	// node
 
 	module.exports = A1W;
+
+	let request = require( "request" );
 
 	A1W.http_get = function( url, cb ) {
 		request( url, ( err, resp, body ) => {
@@ -32,6 +32,23 @@ else {
 		x.send();
 	};
 
+	A1W.http_get_obj = function( url, cb ) {
+		A1W.http_get( url, ( json, err ) => {
+			if( err ) {
+				cb( null, err );
+			}
+			else {
+				let obj = JSON.parse( json );
+				if( obj.error ) {
+					cb( null, obj.error );
+				}
+				else {
+					cb( obj.data, null );
+				}
+			}
+		} );
+	};
+
 }
 
 
@@ -40,13 +57,13 @@ A1W.google = {
 		document: {
 			get: function( doc_id, cb ) {
 				let url = "https://sleepless.com/api/?cmd=google/drive/document&key=" + doc_id;	// XXX endpoint is not final
-				A1W.http_get( url, cb );
+				A1W.http_get_obj( url, cb );
 			},
 		},
 		spreadsheet: {
 			get: function( doc_id, cb ) {
 				let url = "https://sleepless.com/api/?cmd=google/drive/spreadsheet&key=" + doc_id;	// XXX endpoint is not final
-				A1W.http_get( url, cb );
+				A1W.http_get_obj( url, cb );
 			},
 		},
 	},
